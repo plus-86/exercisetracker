fc10b276775e70b33c0dccbe630b564c9955cd23
a3a4aa5 <linlgd@outlook.com>
2022-06-15 02:50:02 +0800

    c

--- index.js
+++ index.js
@@ -19,7 +19,9 @@ const personSchema = new Schema({
   description: { type: String },
   duration: { type: String },
   date: { type: String },
-  timestamp: { type: Number }
+  timestamp: { type: Number },
+  count: { type: Number },
+  logs: { type: Array }
 })
 const Person = mongoose.model('Person', personSchema)
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -40,7 +42,6 @@ const listener = app.listen(process.env.PORT || 3000, () => {
 
 // 创建用户
 app.post('/api/users', (req, res) => {
-  console.log(req.body)
   let p = new Person({
     username: req.body.username
   })
@@ -63,68 +64,45 @@ app.get('/api/users', (req, res) => {
     })
 })
 // 写入description、duration 和 date
-app.post('/api/users/:_id/exercises', (req, res) => {
-  let id = req.body[':_id']
-  console.log(id, typeof id)
-  console.log(req.body.date, 'req.body.date')
-  Person.findByIdAndUpdate(
-    id, // id是object模板里的type改成object才能提交
-    {
-      description: req.body.description,
-      duration: req.body.duration,
-      date: req.body.date
-        ? new Date(req.body.date).toDateString()
-        : new Date().toDateString(),
-      timestamp: req.body.date
-        ? new Date(req.body.date).getTime()
-        : new Date().getTime()
-    },
-    { new: true }
-  )
-    .select({ username: 1, description: 1, duration: 1, date: 1, _id: 1 })
-    .exec((err, data) => {
-      if (err) return console.log(err)
-      res.json(data)
-    })
+app.post('/api/users/:_id/exercises', async (req, res) => {
+  let id = req.params._id // get请求url的参数在query、post请求url的参数在params、post请求的表单提交数据在body
+  let obj = {
+    description: req.body.description,
+    duration: parseInt(req.body.duration),
+    date: req.body.date
+      ? new Date(req.body.date).toDateString()
+      : new Date().toDateString(),
+    timestamp: req.body.date
+      ? new Date(req.body.date).getTime()
+      : new Date().getTime()
+  }
+  Person.findById(id, (err, data) => {
+    if (err) return console.log(err)
+    if (data.count === undefined) {
+      data.logs = []
+      data.logs.push(obj)
+      data.count = 1
+      data.save((err, dt) => {
+        if (err) return console.log(err)
+        res.json(obj)
+      })
+    } else {
+      data.logs.push(obj)
+      data.count++
+      data.save((err, dt) => {
+        if (err) return console.log(err)
+        res.json(obj)
+      })
+    }
+  })
 })
 
 // 获取用户exercise日志
-app.get('/api/users/:_id/logs', (req, res) => {
+app.get('/api/users/:_id/logs', (req, resp) => {
   let id = req.params._id
-  // 传query范围查询其他用户
-  if (req.query.limit !== undefined) {
-    let from = new Date(req.query.from).getTime()
-    let to = new Date(req.query.to).getTime()
-    let limit = req.query.limit
-    console.log(id, 'id', typeof id)
-    console.log(from, 'from')
-    console.log(to, 'to')
-    console.log(limit, 'limit')
-    Person.findById(id)
-      .where('timestamp')
-      .gte(from)
-      .lte(to)
-      .limit(limit)
-      .exec((err, data) => {
-        console.log(data, 'data')
-        if (err) return console.log(err)
-        res.send(data)
-      })
-  } else {
-    // 不传参查自己
-    Person.findById(id).exec((err, data) => {
-      if (err) return console.log(err)
-      res.json({
-        _id: data._id,
-        username: data.username,
-        log: [
-          {
-            description: data.description,
-            duration: parseInt(data.duration),
-            date: data.date
-          }
-        ]
-      })
-    })
-  }
+  // 查用户logs
+  Person.findById(id).exec((err, res) => {
+    if (err) return console.log(err)
+    resp.json({ count: res.count, log: res.logs })
+  })
 })
